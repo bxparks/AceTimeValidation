3d17c2c7b88bfe3c2ffa2eacbdfcf08dee983535
Brian Park <brian@xparks.net>
2023-02-05 23:56:42 -0800

    tools/compare_go: Move transition searching code to transitions.go; similar to AceTimeGo/cmd/validatetime/transitions.go

--- tools/compare_go/generate_data.go
+++ tools/compare_go/generate_data.go
@@ -248,7 +248,7 @@ func addTransitions(
 	tz *time.Location,
 ) []TestItemType {
 
-	transitions := findTransitions(tz)
+	transitions := findTransitions(startYear, untilYear, samplingInterval, tz)
 	for _, transition := range transitions {
 		beforeTestItem := createTestItem(transition.before, "A")
 		afterTestItem := createTestItem(transition.after, "B")
@@ -280,10 +280,10 @@ func createTestItem(t time.Time, itemType string) TestItemType {
 	epochSeconds := int(unixSeconds - secondsToAceTimeEpochFromUnixEpoch)
 
 	// seconds
-	totalOffset := UtcOffsetMinutes(t) * 60
+	totalOffset := utcOffsetMinutes(t) * 60
 	// Go lang time package does not provide the DST offset.
 	dstOffset := 0
-	abbrev := Abbreviation(t)
+	abbrev := abbreviation(t)
 
 	return TestItemType{
 		epochSeconds,
@@ -300,101 +300,7 @@ func createTestItem(t time.Time, itemType string) TestItemType {
 	}
 }
 
-// A tuple that represents the before and after time of a timezone transition.
-type TransitionTimes struct {
-	before time.Time
-	after  time.Time
-}
-
-// findTransitions() finds the timezone transitions and returns an array of
-// tuples of (before, after).
-func findTransitions(tz *time.Location) []TransitionTimes {
-	dt := time.Date(startYear, 1, 1, 0, 0, 0, 0, time.UTC)
-	dtLocal := dt.In(tz)
-
-	transitions := make([]TransitionTimes, 0, 500)
-	samplingIntervalNanos := samplingInterval * 3600 * 1000000000
-	intervalDuration := time.Duration(samplingIntervalNanos)
-	for {
-		dtNext := dt.Add(intervalDuration)
-		dtNextLocal := dtNext.In(tz)
-		if dtNextLocal.Year() >= untilYear {
-			break
-		}
-
-		// Look for utc offset transition
-		if isTransition(dtLocal, dtNextLocal) {
-			dtLeft, dtRight := binarySearchTransition(tz, dt, dtNext)
-			dtLeftLocal := dtLeft.In(tz)
-			dtRightLocal := dtRight.In(tz)
-			transitions = append(
-				transitions, TransitionTimes{dtLeftLocal, dtRightLocal})
-		}
-
-		dt = dtNext
-		dtLocal = dtNextLocal
-	}
-
-	return transitions
-}
-
-func isTransition(before time.Time, after time.Time) bool {
-	return UtcOffsetMinutes(before) != UtcOffsetMinutes(after)
-}
-
-func binarySearchTransition(
-	tz *time.Location,
-	dtLeft time.Time,
-	dtRight time.Time) (time.Time, time.Time) {
-
-	dtLeftLocal := dtLeft.In(tz)
-	for {
-		duration := dtRight.Sub(dtLeft)
-		durationMinutes := int(duration.Minutes())
-		deltaMinutes := durationMinutes / 2
-		if deltaMinutes == 0 {
-			break
-		}
-
-		dtMid := dtLeft.Add(time.Duration(deltaMinutes * 60 * 1000000000))
-		dtMidLocal := dtMid.In(tz)
-		if isTransition(dtLeftLocal, dtMidLocal) {
-			dtRight = dtMid
-		} else {
-			dtLeft = dtMid
-			dtLeftLocal = dtMidLocal
-		}
-	}
-	return dtLeft, dtRight
-}
-
-//-----------------------------------------------------------------------------
-
-// UtcOffsetMinutes() returns the UTC offset of the given time as minutes.
-func UtcOffsetMinutes(t time.Time) int {
-	utcOffsetString := t.Format("-07:00")
-	return convertOffsetStringToMinutes(utcOffsetString)
-}
-
-func convertOffsetStringToMinutes(offset string) int {
-	hourString := offset[0:3]
-	minuteString := offset[4:6]
-	hour, _ := strconv.Atoi(hourString)
-	minute, _ := strconv.Atoi(minuteString)
-	return convertHourMinuteToMinutes(hour, minute)
-}
-
-func convertHourMinuteToMinutes(hour int, minute int) int {
-	sign := 1
-	if hour < 0 {
-		sign = -1
-		hour = -hour
-	}
-	minutes := hour*60 + minute
-	return sign * minutes
-}
-
-func Abbreviation(t time.Time) string {
+func abbreviation(t time.Time) string {
 	abbrev := t.Format("MST")
 	return abbrev
 }
---
+++ tools/compare_go/go.mod
@@ -0,0 +1,3 @@
+module github.com/bxparks/AceTimeValidation/compare_go
+
+go 1.19
